26714a6e9ca6c309e7aed7f70008ba9d87b37f06
Cole Gawin <colegawin@gmail.com>
2020-08-04 02:42:42 -0500

    fix firebase

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { initFirebase } from './firebase/firebase.service';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  await initFirebase();
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-  );
-  app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 3000, '0.0.0.0');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(parseInt(process.env.PORT) || 8080, '0.0.0.0');
 }
-
 bootstrap();
